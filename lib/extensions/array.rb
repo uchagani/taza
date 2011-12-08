3cbeae7684d9cc51d0cad0fd63b40a994333dc2a
Pedro Nascimento <pnascimento@gmail.com>
2011-12-08 19:55:40 -0200

    simplifying array equivalent_to

--- lib/extensions/array.rb
+++ lib/extensions/array.rb
@@ -4,7 +4,6 @@ class Array
   #  [1,2].equivalent([2,1])    # => true
   #  [1,2,3].equivalent([2,1])  # => false
   def equivalent?(other_array)
-    merged_array = self & other_array
-    merged_array.size == self.size && merged_array.size == other_array.size
+    self.sort == other_array.sort
   end
 end
