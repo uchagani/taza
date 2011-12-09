d592a24f6315de824f30e3b304b3bef45f4fa32a
Pedro Nascimento <pnascimento@gmail.com>
2011-12-09 02:00:54 -0200

    Revert "simplifying array equivalent_to" (doesn't work in 1.8 and JRuby)
    This reverts commit 3cbeae7684d9cc51d0cad0fd63b40a994333dc2a.

--- lib/extensions/array.rb
+++ lib/extensions/array.rb
@@ -4,6 +4,7 @@ class Array
   #  [1,2].equivalent([2,1])    # => true
   #  [1,2,3].equivalent([2,1])  # => false
   def equivalent?(other_array)
-    self.sort == other_array.sort
+    merged_array = self & other_array
+    merged_array.size == self.size && merged_array.size == other_array.size
   end
 end
