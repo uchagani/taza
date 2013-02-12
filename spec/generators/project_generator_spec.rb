60b961b03077586af2bcdd03dd45fc53b8b0d768
hillary <hhueter@certona.com>
2013-02-12 13:42:32 -0800

    added a test to validate choosing the browser.

--- spec/generators/project_generator_spec.rb
+++ spec/generators/project_generator_spec.rb
@@ -92,7 +92,46 @@ require 'generator_spec/test_case'
   end
 
 
-  describe New, "argument NAME DRIVER BROWSER"
+  describe New, "argument NAME DRIVER BROWSER" do
+    include GeneratorSpec::TestCase
+    destination TMP_ROOT
+
+    arguments [PROJECT_NAME, 'watir-webdriver', 'ie']
+
+    before :all do
+      prepare_destination
+      run_generator
+    end
+
+    after do
+      bare_teardown
+    end
+
+    specify do
+      destination_root.should have_structure {
+        directory "#{PROJECT_NAME}/config" do
+          file "config.yml" do
+            contains "driver: watir_webdriver"
+            contains "browser: ie"
+          end
+        end
+        directory "#{PROJECT_NAME}/lib" do
+          directory "sites"
+        end
+        directory "#{PROJECT_NAME}/spec" do
+          directory "integration"
+          directory "isolation"
+          directory "story"
+          directory "support"
+          file "spec_helper.rb"
+        end
+        directory "#{PROJECT_NAME}" do
+          file "Gemfile"
+          file "Rakefile"
+        end
+      }
+    end
+  end
 
 
 
