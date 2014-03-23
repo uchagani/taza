e2e12a83c22f1664872eabc1c1706ba0304b7b7c
Oscar Rieken <oriekenjr@gmail.com>
2014-03-23 19:50:39 -0400

    added spec_helper to project create all new specs passing

--- lib/taza/generators/project_generator.rb
+++ lib/taza/generators/project_generator.rb
@@ -16,7 +16,15 @@ module Taza
     desc "This creates the Taza project structure"
     def create
       template('templates/project/Gemfile.tt', 'Gemfile') unless File.exists? 'Gemfile'
+      template('templates/project/Rakefile.tt', 'Rakefile') unless File.exists? 'Rakefile'
+      template('templates/project/config.yml.tt', 'config/config.yml') unless File.exists? 'config/config.yml'
+      empty_directory 'lib/sites'
+      empty_directory 'spec'
+      template('templates/project/spec_helper.rb.tt', 'spec/spec_helper.rb') unless File.exists? 'spec/spec_helper.rb'
+      empty_directory 'spec/isolation'
+      empty_directory 'spec/integration'
+      empty_directory 'bin'
+      template('templates/project/taza.tt', 'bin/taza') unless File.exists? 'bin/taza'
     end
-
   end
 end
