2a87bc7a95b9bbe868ecaebb32771ca5d431e227
Oscar/Pedro <oriekenjr@gmail.com>
2014-03-24 22:01:07 -0400

    refactored partial and flow generator to lessen complexity

--- lib/taza/generators/flow_generator.rb
+++ lib/taza/generators/flow_generator.rb
@@ -2,27 +2,16 @@ require 'thor'
 require 'active_support/all'
 
 module Taza
-  class FlowGenerator < Thor::Group
-    include Thor::Actions
-
-    argument :flow_name
-    argument :site_name
-
-    def self.source_root
-      File.dirname(__FILE__)
-    end
+  class FlowGenerator < PartialGenerator
 
     desc "This will generate your Taza flow. Example: taza flow checkout foo"
     def flow
-      name = site_name.underscore
+      partial
+    end
 
-      if File.directory?("lib/sites/#{name}")
-        template('templates/flow/flow.rb.tt', "lib/sites/#{name}/flows/#{flow_name}.rb")
-      else
-        say "No such site #{name} exists! ", :red
-        say "Please run 'taza create #{name}'", :green
-      end
-
+    private
+    def create_partial
+      template('templates/flow/flow.rb.tt', "lib/sites/#{name}/flows/#{partial_name}.rb")
     end
   end
 end
--- lib/taza/generators/partial_generator.rb
+++ lib/taza/generators/partial_generator.rb
@@ -3,6 +3,7 @@ require 'active_support/all'
 
 module Taza
   class PartialGenerator < Thor::Group
+    attr_reader :name
     include Thor::Actions
 
     argument :partial_name
@@ -14,14 +15,19 @@ module Taza
 
     desc "This will generate your Taza partial. Example: taza partial navigation foo"
     def partial
-      name = site_name.underscore
+      @name = site_name.underscore
 
       if File.directory?("lib/sites/#{name}")
-        template('templates/partial/partial.rb.tt', "lib/sites/#{name}/pages/partials/#{partial_name}.rb")
+        create_partial
       else
         say "No such site #{name} exists! ", :red
         say "Please run 'taza create #{name}'", :green
       end
+    end
+
+    private
+    def create_partial
+      template('templates/partial/partial.rb.tt', "lib/sites/#{name}/pages/partials/#{partial_name}.rb")
     end
   end
 end
